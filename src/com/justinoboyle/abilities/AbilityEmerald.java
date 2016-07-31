6e28e3651f5b8087558cf6e0089c6361c43bb3d7
justinoboyle <justin@justinoboyle.com>
2016-07-31 19:09:14 -0400

    First commit! Hooray!!

--- src/com/justinoboyle/abilities/AbilityEmerald.java
+++ src/com/justinoboyle/abilities/AbilityEmerald.java
@@ -1,10 +1,17 @@
 package com.justinoboyle.abilities;
 
+import java.util.List;
+import java.util.Random;
+
 import me.jrl1004.AbstractAbility;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Effect;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class AbilityEmerald extends AbstractAbility {
 
@@ -16,6 +23,12 @@ public class AbilityEmerald extends AbstractAbility {
 
     @Override
     public void onUse(Player p) {
+        p.teleport(((List<Player>) Bukkit.getOnlinePlayers()).get(new Random().nextInt(Bukkit.getOnlinePlayers().size())));
+        ItemStack i = p.getItemInHand();
+        i.setAmount(Math.max(p.getItemInHand().getAmount() - 1, 0));
+        p.setItemInHand(i);
+        p.playSound(p.getLocation(), Sound.ENDERMAN_TELEPORT, 1, 1);
+        p.playEffect(p.getLocation(), Effect.ENDER_SIGNAL, 1);
         super.onUse(p);
     }
     
--- src/com/justinoboyle/abilities/AbilityJustin.java
+++ src/com/justinoboyle/abilities/AbilityJustin.java
@@ -4,7 +4,7 @@ import me.jrl1004.AbstractAbility;
 
 public class AbilityJustin {
 
-    private static final AbstractAbility[] abilities = { };
+    private static final AbstractAbility[] abilities = { new AbilityEmerald() };
     
     public static void init() {
         for(AbstractAbility a : abilities) {}
